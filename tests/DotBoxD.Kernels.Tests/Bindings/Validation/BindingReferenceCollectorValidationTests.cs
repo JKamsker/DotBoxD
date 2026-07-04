eb5fccfc68d3fabaa40b5b7c51bf0599585b037d
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-07-04 09:43:32 +0200

    Add red tests for binding collector null arguments

---
+++ tests/DotBoxD.Kernels.Tests/Bindings/Validation/BindingReferenceCollectorValidationTests.cs
@@ -0,0 +1,66 @@
+using DotBoxD.Kernels.Bindings;
+using DotBoxD.Kernels.Model;
+using DotBoxD.Kernels.Sandbox;
+
+namespace DotBoxD.Kernels.Tests.Bindings;
+
+public sealed class BindingReferenceCollectorValidationTests
+{
+    [Fact]
+    public void Collect_rejects_null_module_argument()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            BindingReferenceCollector.Collect(null!, EmptyBindings()));
+
+        Assert.Equal("module", ex.ParamName);
+    }
+
+    [Fact]
+    public void CollectByFunction_rejects_null_module_argument()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            BindingReferenceCollector.CollectByFunction(null!, EmptyBindings()));
+
+        Assert.Equal("module", ex.ParamName);
+    }
+
+    [Fact]
+    public void Collect_rejects_null_bindings_argument()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            BindingReferenceCollector.Collect(PureModule(), null!));
+
+        Assert.Equal("bindings", ex.ParamName);
+    }
+
+    [Fact]
+    public void CollectByFunction_rejects_null_bindings_argument()
+    {
+        var ex = Assert.Throws<ArgumentNullException>(() =>
+            BindingReferenceCollector.CollectByFunction(PureModule(), null!));
+
+        Assert.Equal("bindings", ex.ParamName);
+    }
+
+    private static BindingRegistry EmptyBindings() => new([]);
+
+    private static SandboxModule PureModule()
+        => new(
+            "pure-module",
+            SemVersion.One,
+            SandboxLanguage.CurrentVersion,
+            [],
+            [
+                new SandboxFunction(
+                    "main",
+                    IsEntrypoint: true,
+                    [],
+                    SandboxType.Unit,
+                    [
+                        new ReturnStatement(
+                            new LiteralExpression(SandboxValue.Unit, new SourceSpan(0, 0)),
+                            new SourceSpan(0, 0))
+                    ])
+            ],
+            new Dictionary<string, string>());
+}
