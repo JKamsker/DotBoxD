00ed1943a9fa08c958f909bf41d5ee3c8ae250d4
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-07-04 23:08:18 +0200

    Add red tests for verification policy null inputs

---
+++ tests/DotBoxD.Kernels.Tests/Verifier/Core/Policy/VerificationPolicyContractTests.cs
@@ -0,0 +1,39 @@
+using DotBoxD.Kernels.Verifier;
+
+namespace DotBoxD.Kernels.Tests.Verifier.Core;
+
+public sealed class VerificationPolicyContractTests
+{
+    [Fact]
+    public void WithExpectedManifest_rejects_null_identity_at_public_boundary()
+    {
+        var exception = Assert.Throws<ArgumentNullException>(
+            () => VerificationPolicy.BoxedValueDefaults().WithExpectedManifest(null!));
+
+        Assert.Equal("identity", exception.ParamName);
+    }
+
+    [Fact]
+    public void AllowedMembers_rejects_null_entries_at_public_property_boundary()
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(
+            () => _ = VerificationPolicy.BoxedValueDefaults() with
+            {
+                AllowedMembers = new HashSet<string?> { null }!
+            });
+
+        Assert.Equal("AllowedMembers", exception.ParamName);
+    }
+
+    [Fact]
+    public void ForbiddenTypePrefixes_rejects_null_entries_at_public_property_boundary()
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(
+            () => _ = VerificationPolicy.BoxedValueDefaults() with
+            {
+                ForbiddenTypePrefixes = new HashSet<string?> { null }!
+            });
+
+        Assert.Equal("ForbiddenTypePrefixes", exception.ParamName);
+    }
+}
