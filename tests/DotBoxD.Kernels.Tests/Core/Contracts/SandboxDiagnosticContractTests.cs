8096a9b959d2f7f1ecce8bbd30969588ee153e40
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-07-06 06:56:35 +0200

    Add red tests for sandbox diagnostic contracts

---
+++ tests/DotBoxD.Kernels.Tests/Core/Contracts/SandboxDiagnosticContractTests.cs
@@ -0,0 +1,49 @@
+using DotBoxD.Kernels.Model;
+
+namespace DotBoxD.Kernels.Tests.Core.Contracts;
+
+public sealed class SandboxDiagnosticContractTests
+{
+    [Fact]
+    public void Sandbox_diagnostic_rejects_null_code_and_message()
+    {
+        var code = Assert.Throws<ArgumentNullException>(
+            () => new SandboxDiagnostic(null!, "message"));
+        var message = Assert.Throws<ArgumentNullException>(
+            () => new SandboxDiagnostic("E-TEST", null!));
+
+        Assert.Equal("Code", code.ParamName);
+        Assert.Equal("Message", message.ParamName);
+    }
+
+    [Fact]
+    public void Sandbox_diagnostic_rejects_undefined_severity()
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(
+            () => new SandboxDiagnostic("E-TEST", "message", (DiagnosticSeverity)999));
+
+        Assert.Equal("Severity", exception.ParamName);
+    }
+
+    [Fact]
+    public void Source_span_rejects_negative_coordinates()
+    {
+        var line = Assert.ThrowsAny<ArgumentException>(() => new SourceSpan(-1, 0));
+        var column = Assert.ThrowsAny<ArgumentException>(() => new SourceSpan(0, -1));
+
+        Assert.Equal("Line", line.ParamName);
+        Assert.Equal("Column", column.ParamName);
+    }
+
+    [Fact]
+    public void Validation_exception_rejects_null_diagnostics_boundary()
+    {
+        var collection = Assert.Throws<ArgumentNullException>(
+            () => new SandboxValidationException(null!));
+        var element = Assert.ThrowsAny<ArgumentException>(
+            () => new SandboxValidationException([null!]));
+
+        Assert.Equal("diagnostics", collection.ParamName);
+        Assert.Equal("diagnostics", element.ParamName);
+    }
+}
