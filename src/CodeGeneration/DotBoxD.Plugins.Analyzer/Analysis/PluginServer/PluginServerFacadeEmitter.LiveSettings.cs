0e979e042375be67a52dd3a0d14a2399e2e66b24
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-07-07 15:27:31 +0200

    Reject foreign live setting selectors
    Generated plugin server live-settings handles now verify that Set selectors target a public instance property declared on the requested kernel type or one of its base types before accepting the LiveSetting attribute. This prevents lookalike foreign settings from being queued as updates for the installed kernel.

--- src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/PluginServer/PluginServerFacadeEmitter.LiveSettings.cs
+++ src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/PluginServer/PluginServerFacadeEmitter.LiveSettings.cs
@@ -19,6 +19,10 @@ internal static partial class PluginServerFacadeEmitter
         builder.AppendLine("            {");
         builder.AppendLine("                throw new global::System.ArgumentException(\"Live setting expression must select a property.\", nameof(member));");
         builder.AppendLine("            }");
+        builder.AppendLine("            if (!IsKernelProperty(property))");
+        builder.AppendLine("            {");
+        builder.AppendLine("                throw new global::System.ArgumentException($\"Live setting expression must select a public instance property on '{typeof(TKernel).FullName}'.\", nameof(member));");
+        builder.AppendLine("            }");
         builder.AppendLine("            if (!IsLiveSetting(property))");
         builder.AppendLine("            {");
         builder.AppendLine("                throw new global::System.ArgumentException($\"Property '{property.Name}' is not a live setting.\", nameof(member));");
@@ -67,6 +71,12 @@ internal static partial class PluginServerFacadeEmitter
         builder.AppendLine("            _owner.RequireInstalledKernel<TKernel>(_pluginId);");
         builder.AppendLine("            await _owner.RequireControl().UpdateSettingsAsync(_pluginId, updates, atomic, default).ConfigureAwait(false);");
         builder.AppendLine("        }");
+        builder.AppendLine("        private static bool IsKernelProperty(global::System.Reflection.PropertyInfo property)");
+        builder.AppendLine("            => property.DeclaringType is { } declaringType &&");
+        builder.AppendLine("                declaringType.IsAssignableFrom(typeof(TKernel)) &&");
+        builder.AppendLine("                property.GetMethod is { IsPublic: true, IsStatic: false } &&");
+        builder.AppendLine("                property.SetMethod is { IsPublic: true, IsStatic: false } &&");
+        builder.AppendLine("                property.GetIndexParameters().Length == 0;");
         builder.AppendLine("        private static bool IsLiveSetting(global::System.Reflection.PropertyInfo property)");
         builder.AppendLine("            => property.GetCustomAttributes(typeof(global::DotBoxD.Abstractions.LiveSettingAttribute), inherit: true).Length != 0;");
         builder.AppendLine("        private static bool IsRequired(global::System.Reflection.PropertyInfo property)");
