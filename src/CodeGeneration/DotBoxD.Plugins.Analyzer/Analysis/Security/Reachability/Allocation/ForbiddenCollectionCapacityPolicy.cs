9263c57958708059cdece3b070ed5810ddd26277
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-08-10 04:52:45 +0200

    fix: flag concurrent dictionary capacity allocations
    Recognize ConcurrentDictionary constructors with a capacity parameter in the forbidden collection capacity policy. This closes the analyzer reachability gap covered by the regression test while retaining support for parameterless construction.

--- src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Security/Reachability/Allocation/ForbiddenCollectionCapacityPolicy.cs
+++ src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Security/Reachability/Allocation/ForbiddenCollectionCapacityPolicy.cs
@@ -7,6 +7,8 @@ internal static class ForbiddenCollectionCapacityPolicy
 {
     private const string ArrayListTypeName = "System.Collections.ArrayList";
     private const string BitArrayTypeName = "System.Collections.BitArray";
+    private const string ConcurrentDictionaryTypeName =
+        "System.Collections.Concurrent.ConcurrentDictionary<TKey, TValue>";
     private const string DictionaryTypeName = "System.Collections.Generic.Dictionary<TKey, TValue>";
     private const string HashSetTypeName = "System.Collections.Generic.HashSet<T>";
     private const string HashtableTypeName = "System.Collections.Hashtable";
@@ -103,6 +105,7 @@ internal static class ForbiddenCollectionCapacityPolicy
         {
             ArrayListTypeName => ArrayListTypeName,
             ListTypeName => "System.Collections.Generic.List",
+            ConcurrentDictionaryTypeName => "System.Collections.Concurrent.ConcurrentDictionary",
             DictionaryTypeName => "System.Collections.Generic.Dictionary",
             QueueTypeName => "System.Collections.Generic.Queue",
             HashtableTypeName => "System.Collections.Hashtable",
