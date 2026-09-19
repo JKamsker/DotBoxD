a7fd86b3e50a16d19bde677ec010e8c3279cde56
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-19 08:59:15 +0200

    fix: detect IReadOnlySet SetEquals scans
    Recognize IReadOnlySet<T>.SetEquals in the plugin analyzer's collection scan policy so reachable scans receive the existing DBXK001 diagnostic instead of being silently accepted.

--- src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Security/Reachability/Allocation/CollectionScanPolicy.cs
+++ src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Security/Reachability/Allocation/CollectionScanPolicy.cs
@@ -6,6 +6,7 @@ internal static class CollectionScanPolicy
 {
     private const string DictionaryTypeName = "System.Collections.Generic.Dictionary<TKey, TValue>";
     private const string HashSetTypeName = "System.Collections.Generic.HashSet<T>";
+    private const string IReadOnlySetTypeName = "System.Collections.Generic.IReadOnlySet<T>";
     private const string ListTypeName = "System.Collections.Generic.List<T>";
     private const string QueueTypeName = "System.Collections.Generic.Queue<T>";
     private const string PriorityQueueTypeName =
@@ -23,6 +24,7 @@ internal static class CollectionScanPolicy
         {
             (DictionaryTypeName, "TrimExcess", _) => "System.Collections.Generic.Dictionary.TrimExcess",
             (HashSetTypeName, _, _) => GetHashSetDisplayName(method.Name),
+            (IReadOnlySetTypeName, "SetEquals", _) => "System.Collections.Generic.IReadOnlySet.SetEquals",
             (ListTypeName, "TrueForAll", _) => "System.Collections.Generic.List.TrueForAll",
             (QueueTypeName, "TrimExcess", _) => "System.Collections.Generic.Queue.TrimExcess",
             (PriorityQueueTypeName, "TrimExcess", MethodKind.Ordinary) =>
