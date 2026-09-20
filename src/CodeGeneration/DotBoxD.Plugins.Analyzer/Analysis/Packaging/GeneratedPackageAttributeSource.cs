8136c826d3f284ec7cffd6a312ecc1875ab5909d
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-20 15:05:09 +0200

    fix: preserve generated package platform contracts
    Project SupportedOSPlatform metadata from plugin kernels onto their generated package types so consumers receive the same platform compatibility diagnostics.

--- src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Packaging/GeneratedPackageAttributeSource.cs
+++ src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Packaging/GeneratedPackageAttributeSource.cs
@@ -6,23 +6,29 @@ namespace DotBoxD.Plugins.Analyzer.Analysis;
 internal static class GeneratedPackageAttributeSource
 {
     private const string ExperimentalAttribute = "System.Diagnostics.CodeAnalysis.ExperimentalAttribute";
+    private const string SupportedOSPlatformAttribute = "System.Runtime.Versioning.SupportedOSPlatformAttribute";
 
     public static EquatableArray<string> FromKernel(INamedTypeSymbol kernelType, Compilation compilation)
     {
         var attributes = new List<string>();
         foreach (var attribute in kernelType.GetAttributes())
         {
-            if (IsFrameworkExperimentalAttribute(attribute, compilation) &&
+            if (IsFrameworkAttribute(attribute, compilation, ExperimentalAttribute) &&
                 TryExperimentalAttribute(attribute) is { } source)
             {
                 attributes.Add(source);
+            }
+            else if (IsFrameworkAttribute(attribute, compilation, SupportedOSPlatformAttribute) &&
+                     TrySupportedOSPlatformAttribute(attribute) is { } platformSource)
+            {
+                attributes.Add(platformSource);
             }
         }
 
         return EquatableArray<string>.FromOwned([.. attributes]);
     }
 
-    private static bool IsFrameworkExperimentalAttribute(AttributeData attribute, Compilation compilation)
+    private static bool IsFrameworkAttribute(AttributeData attribute, Compilation compilation, string metadataName)
     {
         foreach (var reference in compilation.References)
         {
@@ -35,7 +41,7 @@ internal static class GeneratedPackageAttributeSource
             if (compilation.GetAssemblyOrModuleSymbol(reference) is IAssemblySymbol assembly &&
                 SymbolEqualityComparer.Default.Equals(
                     attribute.AttributeClass,
-                    assembly.GetTypeByMetadataName(ExperimentalAttribute)))
+                    assembly.GetTypeByMetadataName(metadataName)))
             {
                 return true;
             }
@@ -58,6 +64,18 @@ internal static class GeneratedPackageAttributeSource
         AppendUrlFormat(builder, attribute);
         builder.Append(")]");
         return builder.ToString();
+    }
+
+    private static string? TrySupportedOSPlatformAttribute(AttributeData attribute)
+    {
+        if (attribute.ConstructorArguments.Length != 1 ||
+            attribute.ConstructorArguments[0].Value is not string platformName)
+        {
+            return null;
+        }
+
+        return "[global::System.Runtime.Versioning.SupportedOSPlatformAttribute(" +
+               LiteralReader.StringLiteral(platformName) + ")]";
     }
 
     private static void AppendUrlFormat(StringBuilder builder, AttributeData attribute)
