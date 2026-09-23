76c40702ca878a47866f85ceba976f5fe00516df
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-24 00:56:47 +0200

    fix: preserve plugin package code requirements
    Copy framework RequiresDynamicCode and RequiresUnreferencedCode attributes from plugin kernels to generated package types so consumers retain the kernel's trimming and AOT contracts.

--- src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Packaging/GeneratedPackageAttributeSource.cs
+++ src/CodeGeneration/DotBoxD.Plugins.Analyzer/Analysis/Packaging/GeneratedPackageAttributeSource.cs
@@ -6,6 +6,8 @@ namespace DotBoxD.Plugins.Analyzer.Analysis;
 internal static class GeneratedPackageAttributeSource
 {
     private const string ExperimentalAttribute = "System.Diagnostics.CodeAnalysis.ExperimentalAttribute";
+    private const string RequiresDynamicCodeAttribute = "System.Diagnostics.CodeAnalysis.RequiresDynamicCodeAttribute";
+    private const string RequiresUnreferencedCodeAttribute = "System.Diagnostics.CodeAnalysis.RequiresUnreferencedCodeAttribute";
 
     public static EquatableArray<string> FromKernel(INamedTypeSymbol kernelType, Compilation compilation)
     {
@@ -17,12 +19,23 @@ internal static class GeneratedPackageAttributeSource
             {
                 attributes.Add(source);
             }
+
+            if (TryCodeRequirementAttribute(attribute, compilation) is { } codeRequirementSource)
+            {
+                attributes.Add(codeRequirementSource);
+            }
         }
 
         return EquatableArray<string>.FromOwned([.. attributes]);
     }
 
     private static bool IsFrameworkExperimentalAttribute(AttributeData attribute, Compilation compilation)
+        => IsFrameworkAttribute(attribute, compilation, ExperimentalAttribute);
+
+    private static bool IsFrameworkAttribute(
+        AttributeData attribute,
+        Compilation compilation,
+        string attributeMetadataName)
     {
         foreach (var reference in compilation.References)
         {
@@ -35,7 +48,7 @@ internal static class GeneratedPackageAttributeSource
             if (compilation.GetAssemblyOrModuleSymbol(reference) is IAssemblySymbol assembly &&
                 SymbolEqualityComparer.Default.Equals(
                     attribute.AttributeClass,
-                    assembly.GetTypeByMetadataName(ExperimentalAttribute)))
+                    assembly.GetTypeByMetadataName(attributeMetadataName)))
             {
                 return true;
             }
@@ -60,6 +73,27 @@ internal static class GeneratedPackageAttributeSource
         return builder.ToString();
     }
 
+    private static string? TryCodeRequirementAttribute(AttributeData attribute, Compilation compilation)
+    {
+        var attributeMetadataName = attribute.AttributeClass?.ToDisplayString();
+        if (attributeMetadataName is not (RequiresDynamicCodeAttribute or RequiresUnreferencedCodeAttribute) ||
+            !IsFrameworkAttribute(attribute, compilation, attributeMetadataName) ||
+            attribute.ConstructorArguments.Length != 1 ||
+            attribute.ConstructorArguments[0].Value is not string message)
+        {
+            return null;
+        }
+
+        var builder = new StringBuilder();
+        builder.Append("[global::")
+            .Append(attributeMetadataName)
+            .Append("(")
+            .Append(LiteralReader.StringLiteral(message));
+        AppendUrl(builder, attribute);
+        builder.Append(")]");
+        return builder.ToString();
+    }
+
     private static void AppendUrlFormat(StringBuilder builder, AttributeData attribute)
     {
         foreach (var argument in attribute.NamedArguments)
@@ -67,6 +101,17 @@ internal static class GeneratedPackageAttributeSource
             if (argument.Key == "UrlFormat" && argument.Value.Value is string urlFormat)
             {
                 builder.Append(", UrlFormat = ").Append(LiteralReader.StringLiteral(urlFormat));
+            }
+        }
+    }
+
+    private static void AppendUrl(StringBuilder builder, AttributeData attribute)
+    {
+        foreach (var argument in attribute.NamedArguments)
+        {
+            if (argument.Key == "Url" && argument.Value.Value is string url)
+            {
+                builder.Append(", Url = ").Append(LiteralReader.StringLiteral(url));
             }
         }
     }
