a7c33ce34599c6c589305045976de34f0253ebf1
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-25 22:33:34 +0000

    Serialize named pipe server lifecycle transitions

--- eng/scripts/run-required-tests.ps1
+++ eng/scripts/run-required-tests.ps1
@@ -212,6 +212,7 @@ $defaultMinimums = @{
     SingleConnectionTransportCoverageTests = 8
     SingleConnectionServerTransportLifecycleRegressionTests = 5
     NamedPipeServerTransportCancellationRegressionTests = 2
+    NamedPipeServerLifecycleRaceTests = 2
     UnsupportedShapeCoverageTests = 8
     UnsupportedDtoConstructibilityTests = 1
     UnsupportedDtoPayloadShapeTests = 5
