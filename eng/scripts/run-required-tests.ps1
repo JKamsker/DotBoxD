e9184a1a2eaec1d2a6438c20092bfb38edf5f697
Jonas Kamsker <11245306+JKamsker@users.noreply.github.com>
2026-09-25 21:02:44 +0000

    Isolate stream cancellation callback failures during cleanup

--- eng/scripts/run-required-tests.ps1
+++ eng/scripts/run-required-tests.ps1
@@ -198,6 +198,7 @@ $defaultMinimums = @{
     InstanceRegistryTests = 8
     InstanceRegistryLeaseRegressionTests = 4
     RpcStreamHandleEnvelopeValidationTests = 13
+    RpcStreamCancellationCallbackTests = 3
     UnknownStreamComplete_ReportsProtocolError = 1
     UnknownStreamError_ReportsProtocolError = 1
     ForwardedRemoteStreamError_PreservesOriginalRemoteType = 1
